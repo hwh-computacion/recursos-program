f3eb9e746faa10beeb756487cb104bc04040826e
Computacion <computacion@ithwh.edu.gt>
2020-02-24 14:40:53 -0600

    No hay modificaciones

--- datos.js
+++ datos.js
@@ -2,3 +2,4 @@ var dato;
 dato = prompt("Ingrese un numero:")
 document.write("El numero ingresado es: "+dato);
 console.log("El numero ingresado es: "+dato);
+alert("El numero ingresado es: "+dato);
